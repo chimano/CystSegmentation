452ae80ff4d1ce4184ea18e60415c7e58b6500d0
Chirac <chiracmanoukian@hotmail.com>
2019-11-16 20:40:11 -0500

    blah blah

--- main.py
+++ main.py
@@ -6,17 +6,21 @@ import mpmath
 import numpy as np
 import scipy.io
 from abel.tools.center import find_center
+from abel.tools.math import fit_gaussian
+from scipy import exp
 from scipy.optimize import curve_fit
 
 
 def project_into_columns(img):
     # Need to understand how the paper did this part
-    mat = np.mat(img)
-    return mat*np.linalg.inv(mat.transpose()*mat)*mat
+    inverted = 255 - img
+    proj = np.sum(inverted, 1)
+    return proj
 
 
-if os.path.isdir('./output'):
-    os.mkdir('output')
+def gaus(x, a, x0, sigma):
+    return a*exp(-(x-x0)**2/(2*sigma**2))
+
 
 for i in range(1, 10):
     mat = scipy.io.loadmat(f'2015_BOE_Chiu/Subject_0{i}.mat')
@@ -25,5 +29,6 @@ for i in range(1, 10):
     for j in range(len(images[0][0])):
         newimg = cv2.resize(images[:, :, j], (512, 256))
         newfluid = cv2.resize(manualfluid[:, :, j], (512, 256))
-
+        projection = project_into_columns(newimg)
+        xc = fit_gaussian(projection)[1]
         cv2.imwrite(f'output/Subject_0{i}_{j}.png', newimg)
