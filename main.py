a306644607f616465bb0760086357a6f1e022886
Chirac Manoukian <chiracmanoukian@hotmail.com>
2019-11-21 21:29:22 -0500

    Changed denoiser

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ import matplotlib.pyplot as plt
 import mpmath
 import numpy as np
 import scipy.io
-from abel.tools.center import find_center
+from abel.tools.center import fit_gaussian
 from scipy.optimize import curve_fit
 from tvd import TotalVariationDenoising
 from skimage.transform import radon
+from skimage.restoration import denoise_tv_chambolle
 
 
 def project_into_columns(img):
@@ -44,9 +45,10 @@ for i in range(1, 11):
 
             a = int(fit_gaussian(projection)[1])
             roi = extract_roi(newimg, a)
-            denoised = TotalVariationDenoising(roi).generate()
+            denoised = denoise_tv_chambolle(
+                roi, weight=0.1) * 255
             cv2.imwrite(f'output/Subject_{i:02}_{j:02}.png', newimg)
-            cv2.imwrite(f'output/Subject_{i:02}_{j:02}_denoised.png', roi)
+            cv2.imwrite(f'output/Subject_{i:02}_{j:02}_roi.png', roi)
             cv2.imwrite(f'output/Subject_{i:02}_{j:02}_denoised.png', denoised)
         except:
             pass
