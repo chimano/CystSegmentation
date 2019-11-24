9c1302eb33632b48c440c30f676907647d017d30
Chirac Manoukian <chiracmanoukian@hotmail.com>
2019-11-23 19:10:51 -0500

    Ground truth get generated and saved at gt_output

--- main.py
+++ main.py
@@ -26,28 +26,48 @@ def extract_roi(img, xc, width=250):
     return img[:, xc - width//2: xc + width//2]
 
 
-def gaus(x, a, x0, sigma):
-    return a*exp(-(x-x0)**2/(2*sigma**2))
+def generate_ground_truth(mf1, mf2):
+    mf1[mf1 > 0] = 1.0
+    mf2[mf2 > 0] = 1.0
+    ground_truth = np.zeros(mf1.shape)
+    for i in range(len(ground_truth)):
+        for j in range(len(ground_truth[i])):
+            ground_truth[i, j] = 1.0 if mf1[i,
+                                            j] == 1.0 and mf2[i, j] == 1.0 else 0.0
+    return ground_truth
 
 
 if not os.path.isdir('./output'):
     os.mkdir('output')
+if not os.path.isdir('./gt_output'):
+    os.mkdir('gt_output')
+
 for i in range(1, 11):
     mat = scipy.io.loadmat(f'2015_BOE_Chiu/Subject_{i:02}.mat')
     images = mat['images']
-    manualfluid = mat['manualFluid1']
+    grader1 = mat['manualFluid1']
+    grader2 = mat['manualFluid1']
     for j in range(len(images[0][0])):
+        mf1 = np.nan_to_num(cv2.resize(
+            grader1[:, :, j], (512, 256)), nan=0.0)
+        mf2 = np.nan_to_num(cv2.resize(
+            grader1[:, :, j], (512, 256)), nan=0.0)
+
         newimg = cv2.resize(images[:, :, j], (512, 256))
-        newfluid = cv2.resize(manualfluid[:, :, j], (512, 256))
         projection = project_into_columns(newimg)
         try:
 
-            a = int(fit_gaussian(projection)[1])
-            roi = extract_roi(newimg, a)
+            center = int(fit_gaussian(projection)[1])
+            roi = extract_roi(newimg, center)
             denoised = denoise_tv_chambolle(
-                roi, weight=0.1) * 255
+                roi, weight=0.08) * 255
             cv2.imwrite(f'output/Subject_{i:02}_{j:02}.png', newimg)
             cv2.imwrite(f'output/Subject_{i:02}_{j:02}_roi.png', roi)
             cv2.imwrite(f'output/Subject_{i:02}_{j:02}_denoised.png', denoised)
+            if np.count_nonzero(mf1) and np.count_nonzero(mf2):
+                ground_truth = generate_ground_truth(mf1, mf2) * 255
+                gt_roi = extract_roi(ground_truth, center)
+                cv2.imwrite(
+                    f'gt_output/Subject_{i:02}_{j:02}.png', gt_roi)
         except:
             pass
